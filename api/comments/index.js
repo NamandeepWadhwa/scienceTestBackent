35e13b48c935a8cf3d6845b3ac3e536e0a9dd357
Namandeep Singh Wadhwa <naman23156@gmail.com>
2025-04-10 11:58:30 -0400

    route added for adding the comment

--- api/comments/index.js
+++ api/comments/index.js
@@ -1,7 +1,11 @@
 const getBlogComments=require("./getBlogComments");
+const authenticate = require("../../lib/tokens/authenticate");
+const addComment=require("./addingComment");
 const express=require("express");
 const router=express.Router();
 
+
 router.get("/getBloComments",getBlogComments);
+router.get("/addComment", authenticate, addComment);
 
 module.exports=router;
