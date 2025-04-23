a625dc1e4104d8fc02f38d59deda2d60328dbd81
Namandeep Singh Wadhwa <naman23156@gmail.com>
2025-04-22 23:08:09 -0400

    bugs fixed

--- api/comments/getuserComment.js
+++ api/comments/getuserComment.js
@@ -3,9 +3,10 @@ module.exports=async (req,res)=>{
   try{
     const userId=req.user.id;
     if(!userId)return res.status(400).json({message:"User Id/token required"});
-    const {cursorId}=req.query;
-    console.log(req.query);
-    const comments=await getUserComments(userId,cursorId);
+    const { curosrId } = req.query;
+    console.log(curosrId);
+   
+    const comments = await getUserComments(userId, curosrId);
     const data={
       comments:comments,
       cursorId:comments.length>0?comments[comments.length-1].id:null,
