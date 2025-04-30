70e2704ca4fa7eeee94e5779edffdf1e2dc4a5a4
Namandeep Singh Wadhwa <naman23156@gmail.com>
2025-04-30 14:41:39 -0400

    schema added for the messaging

--- socket.js
+++ socket.js
@@ -1,4 +1,7 @@
 const { Server } = require("socket.io");
+const jwt=require("jsonwebtoken");
+const { emit } = require(".");
+const { hashEmail } = require("./lib/hashing/hash");
 
 
 function setUpSocket(server)
@@ -7,8 +10,27 @@ function setUpSocket(server)
   cors:"*",
   methods:['GET','POST']
   });
+  io.use((socket,next)=>{
+    const token=socket.handshake.auth.token;
+    if(!token) throw new Error("Unauthorized");
+    try{
+    const decoded=jwt.verify(token, process.env.JWT_SECRET);
+    if(!decoded) throw new Error("Unauthorized");
+    socket.user={
+      id:decoded.id,
+      email:decoded.email
+    };
+    console.log(socket.user);
+    next();
+  }
+  catch(err){
+    console.log(err);
+    next(new Error("Eror in token"));
+  }
+
+  })
   io.on("connection",(socket)=>{
-    console.log(socket);
+    
     console.log(socket.id);
   })
 }
